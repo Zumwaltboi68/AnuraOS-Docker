8eafbc4be6883d93fbd754778fc951bbce4f977b
DogeSploitDev <133384144+Zumwaltboi68@users.noreply.github.com>
2024-09-18 17:02:14 -0400

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+# Start the Docker daemon
+dockerd &
+
+# Wait for Docker daemon to start
+while (! docker stats --no-stream ); do
+    echo "Waiting for Docker to launch..."
+    sleep 1
+done
+
+# Now execute the passed command
+exec "$@"
