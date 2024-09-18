5ec66b16638b3fe8379ec4795b8859411221eef4
DogeSploitDev <133384144+Zumwaltboi68@users.noreply.github.com>
2024-09-18 17:06:35 -0400

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -1,14 +1,24 @@
-
 #!/bin/bash
 
-# Start the Docker daemon
+# Start the Docker daemon in the background
 dockerd &
 
-# Wait for Docker daemon to start
+# Wait for Docker daemon to be ready
 while (! docker stats --no-stream ); do
     echo "Waiting for Docker to launch..."
     sleep 1
 done
 
-# Now execute the passed command
+# Run 'make all' first
+make all
+
+# Run 'make rootfs' with auto input of "2" using expect
+/usr/bin/expect <<EOF
+spawn make rootfs
+expect "Choose an option"
+send "2\r"
+expect eof
+EOF
+
+# Continue to execute the passed commands
 exec "$@"
